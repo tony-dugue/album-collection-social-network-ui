ff7f35244116885d674c36775ff578c62e2612f1
Tony Dugué <duguetony@gmail.com>
2024-12-03 19:16:06 +0100

    feat(release): create the release module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,7 @@ const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'activate-account', component: ActivateAccountComponent },
+  { path: 'releases', loadChildren: () => import('./features/release/release.module').then(m => m.ReleaseModule) }
 ];
 
 @NgModule({
---
+++ src/app/features/release/release.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ReleaseRoutingModule } from './release-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ReleaseRoutingModule
+  ]
+})
+export class ReleaseModule { }
