4242a56dde51fc303522c347ceeb972ecd168588
Tony Dugué <duguetony@gmail.com>
2024-12-06 21:05:21 +0100

    feat(release): implement the pagination

--- src/app/features/release/pages/release-list/release-list.component.ts
+++ src/app/features/release/pages/release-list/release-list.component.ts
@@ -9,9 +9,10 @@ import { PageResponseReleaseResponse } from '../../../../services/models';
   styleUrl: './release-list.component.scss'
 })
 export class ReleaseListComponent implements OnInit {
+
   releaseResponse: PageResponseReleaseResponse = {};
   page: number = 0;
-  size: number = 5;
+  size: number = 1;
 
   constructor(
     private releaseService: ReleaseService,
@@ -19,15 +20,44 @@ export class ReleaseListComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.findAllBooks();
+    this.findAllReleases();
   } 
 
-  findAllBooks() {
+  findAllReleases() {
     this.releaseService.findAllReleases({ page: this.page, size: this.size })
       .subscribe({
         next: (releases): void => {
           this.releaseResponse = releases;
         }
       });
+  }
+
+  gotToPage(page: number) {
+    this.page = page;
+    this.findAllReleases();
+  }
+
+  goToFirstPage() {
+    this.page = 0;
+    this.findAllReleases();
+  }
+
+  goToPreviousPage() {
+    this.page --;
+    this.findAllReleases();
+  }
+
+  goToLastPage() {
+    this.page = this.releaseResponse.totalPages as number - 1;
+    this.findAllReleases();
+  }
+
+  goToNextPage() {
+    this.page++;
+    this.findAllReleases();
+  }
+
+  get isLastPage(): boolean {
+    return this.page === this.releaseResponse.totalPages as number - 1;
   }
 }
