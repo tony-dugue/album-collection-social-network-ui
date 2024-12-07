ac3b47596bac66435a91c65e96b9371a1c6ef8bc
Tony Dugué <duguetony@gmail.com>
2024-12-07 10:16:47 +0100

    feat(release): implement the borrow action

--- src/app/features/release/pages/release-list/release-list.component.ts
+++ src/app/features/release/pages/release-list/release-list.component.ts
@@ -1,34 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ReleaseService } from '../../../../services/services';
-import { PageResponseReleaseResponse } from '../../../../services/models';
+import {
+  PageResponseReleaseResponse,
+  ReleaseResponse,
+} from '../../../../services/models';
 
 @Component({
   selector: 'app-release-list',
   templateUrl: './release-list.component.html',
-  styleUrl: './release-list.component.scss'
+  styleUrl: './release-list.component.scss',
 })
 export class ReleaseListComponent implements OnInit {
-
   releaseResponse: PageResponseReleaseResponse = {};
   page: number = 0;
-  size: number = 1;
+  size: number = 3;
+  message: string = '';
+  level: 'success' |'error' = 'success';
 
-  constructor(
-    private releaseService: ReleaseService,
-    private router: Router
-  ) {}
+  constructor(private releaseService: ReleaseService, private router: Router) {}
 
   ngOnInit(): void {
     this.findAllReleases();
-  } 
+  }
 
   findAllReleases() {
-    this.releaseService.findAllReleases({ page: this.page, size: this.size })
+    this.releaseService
+      .findAllReleases({ page: this.page, size: this.size })
       .subscribe({
         next: (releases): void => {
           this.releaseResponse = releases;
-        }
+        },
       });
   }
 
@@ -43,12 +45,12 @@ export class ReleaseListComponent implements OnInit {
   }
 
   goToPreviousPage() {
-    this.page --;
+    this.page--;
     this.findAllReleases();
   }
 
   goToLastPage() {
-    this.page = this.releaseResponse.totalPages as number - 1;
+    this.page = (this.releaseResponse.totalPages as number) - 1;
     this.findAllReleases();
   }
 
@@ -58,6 +60,26 @@ export class ReleaseListComponent implements OnInit {
   }
 
   get isLastPage(): boolean {
-    return this.page === this.releaseResponse.totalPages as number - 1;
+    return this.page === (this.releaseResponse.totalPages as number) - 1;
+  }
+
+  borrowRelease(release: ReleaseResponse) {
+    this.message = '';
+    this.level = 'success';
+    this.releaseService
+      .borrowRelease({
+        'release-id': release.id as number,
+      })
+      .subscribe({
+        next: () => {
+          this.level = 'success';
+          this.message = 'Release successfully added to your list';
+        },
+        error: (err) => {
+          console.log(err);
+          this.level = 'error';
+          this.message = err.error.error;
+        },
+      });
   }
 }
